11122b61e5129b9888161ff362c0d38482215dd6
Marco E <marco.esposito@gmail.com>
2016-03-29 21:03:43 +0200

    removed encoder.h

--- encoder.h
+++
@@ -1,8 +0,0 @@
-#ifndef __ENCODER_H__
-#define __ENCODER_H__
-
-void encoder_init(void);
-void encoder_pulse(int gpio, int lev, uint32_t tick);
-void encoder_button(int gpio, int lev, uint32_t tick);
-
-#endif
